7a135782206efc5c040162d665fb1864cad0ad6b
nathanieldavidson <ndavidson@csumb.edu>
2016-10-25 15:44:41 -0700

    initial circleci test

---
+++ Index.php
@@ -0,0 +1,2 @@
+<?php
+include "lib/Monster.php";
---
+++ tests/MonsterTest.php
@@ -0,0 +1,20 @@
+<?php
+use PHPUnit\Framework\TestCase;
+/**
+ * Created by PhpStorm.
+ * User: nathanieldavidson
+ * Date: 10/25/16
+ * Time: 3:25 PM
+ */
+include "lib/Monster.php";
+class MonsterTest extends TestCase
+{
+    public function testThisMethod()
+    {
+        // Arrange
+        $a = new Monster();
+
+        // Assert
+        $this->assertEquals("werewolf", $a->pop());
+    }
+}
